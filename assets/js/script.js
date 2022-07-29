1154b1027c9bd96085f15896838638f26a394545
Jaxon Adams <jadamsresume1@gmail.com>
2022-07-29 17:48:09 -0600

    drew line using canvas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-console.log('Script connected.');
+// Get canvas from DOM
+const canvas = document.querySelector('#drawing-canvas');
+
+// Set canvas size
+const docWidth = document.body.clientWidth;
+const docHeight = document.body.clientHeight;
+canvas.height = docHeight;
+canvas.width = docWidth;
+
+// Set up drawing object
+const ctx = canvas.getContext('2d');
+
+// test draw line
+ctx.moveTo(0, 0);
+ctx.lineTo(100, 100);
+ctx.stroke();
