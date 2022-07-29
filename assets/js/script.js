0aea6040b07be772f361897f7917c020978c3806
Jaxon Adams <jadamsresume1@gmail.com>
2022-07-29 17:54:48 -0600

    began work on drawing line with user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
-// Get canvas from DOM
+// get canvas from DOM
 const canvas = document.querySelector('#drawing-canvas');
 
-// Set canvas size
+// set canvas size
 const docWidth = document.body.clientWidth;
 const docHeight = document.body.clientHeight;
 canvas.height = docHeight;
 canvas.width = docWidth;
 
-// Set up drawing object
+// set up drawing object
 const ctx = canvas.getContext('2d');
 
-// test draw line
-ctx.moveTo(0, 0);
-ctx.lineTo(100, 100);
-ctx.stroke();
+// is user drawing (false by default)
+let isDrawing = false;
+
+// mouse down/up functions
+const mouseDown = () => {
+    isDrawing = true;
+};
+
+const mouseUp = () => {
+    isDrawing = false;
+};
+
+// event listeners
+canvas.addEventListener('mousedown', mouseDown);
+canvas.addEventListener('mouseup', mouseUp);
