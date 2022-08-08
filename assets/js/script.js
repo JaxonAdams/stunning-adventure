6150f2158981c2d0c55af2e9f75c7254522a050d
Jaxon Adams <jadamsresume1@gmail.com>
2022-08-08 09:50:32 -0600

    set up reset btn and color changing functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 // get canvas from DOM
 const canvas = document.querySelector('#drawing-canvas');
 
+// get other elements from DOM
+const resetBtn = document.querySelector('.reset-btn');
+const clrBlack = document.querySelector('.color-black');
+const clrRed = document.querySelector('.color-red');
+const clrBlue = document.querySelector('.color-blue');
+const clrGreen = document.querySelector('.color-green');
+
 // set canvas size
 const docWidth = document.body.clientWidth;
     // -50 takes into account the 50px header
@@ -14,7 +21,6 @@ const ctx = canvas.getContext('2d');
 // line properties
 ctx.lineWidth = 10;
 ctx.lineCap = 'round';
-ctx.strokeStyle = 'black';
 
 // is user drawing (false by default)
 let isDrawing = false;
@@ -44,3 +50,10 @@ const draw = e => {
 canvas.addEventListener('mousedown', mouseDown);
 canvas.addEventListener('mouseup', mouseUp);
 canvas.addEventListener('mousemove', draw);
+
+resetBtn.addEventListener('click', () => window.location.reload());
+
+clrBlack.addEventListener('click', () => ctx.strokeStyle = 'black');
+clrRed.addEventListener('click', () => ctx.strokeStyle = 'red');
+clrBlue.addEventListener('click', () => ctx.strokeStyle = 'blue');
+clrGreen.addEventListener('click', () => ctx.strokeStyle = 'green');
