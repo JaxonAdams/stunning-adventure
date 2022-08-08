3fb12f1f616987054434092648ec4328dd9433b2
Jaxon Adams <jadamsresume1@gmail.com>
2022-08-08 09:09:02 -0600

    basic drawing set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,25 @@ const ctx = canvas.getContext('2d');
 let isDrawing = false;
 
 // mouse down/up functions
-const mouseDown = () => {
+const mouseDown = e => {
     isDrawing = true;
+    ctx.beginPath();
 };
 
 const mouseUp = () => {
     isDrawing = false;
 };
 
+const draw = e => {
+    if (isDrawing) {
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    };
+};
+
 // event listeners
 canvas.addEventListener('mousedown', mouseDown);
 canvas.addEventListener('mouseup', mouseUp);
+canvas.addEventListener('mousemove', draw);
